28a4d73ca6bd70acc16b4209e9d830664cc01133
aluno <aluno@example.com>
2019-05-30 20:23:23 -0300

    cria modelo Medicamento

--- atendimento/admin.py
+++ atendimento/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from . models import Paciente
 from . models import Medico
+from . models import Medicamento
 
 admin.site.register(Paciente)
 admin.site.register(Medico)
+admin.site.register(Medicamento)
